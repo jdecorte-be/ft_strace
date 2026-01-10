3b4e3a8db1c0753ee4da957ea27c1a6a250d6336
John <john.decorte01@gmail.com>
2026-01-10 22:21:48 +0100

    clean header

--- includes/strace.h
+++ includes/strace.h
@@ -1,53 +1,55 @@
 #ifndef STRACE_H
-# define STRACE_H
+#define STRACE_H
 
 #define _GNU_SOURCE
-# include <elf.h>
-# include <errno.h>
-# include <fcntl.h>
-# include <sched.h>
-# include <stdarg.h>
-# include <stdbool.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <string.h>
-# include <sys/ptrace.h>
-# include <sys/stat.h>
-# include <sys/uio.h>
-# include <sys/user.h>
-# include <sys/wait.h>
-# include <unistd.h>
-# include <argp.h>
-# include <signal.h>
-# include <error.h>
+#include <argp.h>
+#include <elf.h>
+#include <errno.h>
+#include <error.h>
+#include <fcntl.h>
+#include <sched.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
-# include <time.h>
-# include <sys/time.h>
+/* System headers */
+#include <sys/ptrace.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/user.h>
+#include <sys/wait.h>
 
-# include "syscalls.h"
+#include "syscalls.h"
 
-# define BUFFER_SIZE 4096
-# define MAX_ARGS 6
+#define BUFFER_SIZE 4096
+#define MAX_ARGS 6
 
-typedef struct s_stats {
+typedef struct s_stats
+{
     unsigned long long calls;
     unsigned long long errors;
-    struct timespec    total_time;
-    const char         *name;
+    struct timespec total_time;
+    const char *name;
 } t_stats;
 
 typedef enum e_sys_arch
 {
     ARCH_X86_64, // 64-bit architecture
-    ARCH_I386,    // 32-bit architecture
+    ARCH_I386,   // 32-bit architecture
     ARCH_UNKNOWN,
-}   t_sys_arch;
+} t_sys_arch;
 
 struct arguments
 {
     char **target;
     _Bool sum_opt;
-
 };
 
 typedef struct s_strace
@@ -59,15 +61,16 @@ typedef struct s_strace
 
     pid_t pid;
     size_t n_env;
-}   t_strace;
+} t_strace;
 
-typedef struct	syscall_s {
-	char		*name;
-	int			argc;
-	int			type_args[MAX_ARGS];
-	int			type_ret;
+typedef struct syscall_s
+{
+    char *name;
+    int argc;
+    int type_args[MAX_ARGS];
+    int type_ret;
 
-}				syscall_t;
+} syscall_t;
 
 void print_summary(t_strace *strace);
 void print_syscall(t_strace *strace, syscall_t syscall, int argc, ...);
